fac19fdc0a725ab9f07c3e4d218640fd0ab392b5
Iv44n <ivandev2oo6@gmail.com>
2024-10-05 15:30:10 -0500

    Creating product interface

---
+++ server/src/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  id: number
+  name: string
+  description: string
+  price: string
+  stock_quantity: number
+  category_id: number
+  brand_id: number
+  image_url: string
+  created_at: string
+  rating: string
+}
