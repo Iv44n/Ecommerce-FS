620f2810c1327f497fe85aac3f80070a4d945881
Iv44n <ivandev2oo6@gmail.com>
2024-10-05 15:30:46 -0500

    Refactored ProductService to include pagination and type annotations

--- server/src/services/productService.ts
+++ server/src/services/productService.ts
@@ -1,12 +1,20 @@
 import { ProductModel } from '../models/productModel'
+import { Product } from '../types/product'
 
 export class ProductService {
 
-  static async getAllProducts() {
-    return await ProductModel.getAllProducts()
+  static async getAllProducts({ limit = 10, offset = 0 }: { limit?: number, offset?: number }): Promise<Product[]> {
+    return await ProductModel.getAllProducts({
+      limit,
+      offset
+    })
   }
 
-  static async getProduct({ id }: { id: string }) {
+  static async getProduct({ id }: { id: string }): Promise<Product> {
+    if (!id) {
+      throw new Error('Missing id')
+    }
+
     return ProductModel.getProduct({ id })
   }
 }
