9506472d94738080a754b59f72059d762731c1fe
Iv44n <ivandev2oo6@gmail.com>
2024-10-12 18:04:46 -0500

    Removed search form from ShopPage for a cleaner layout

--- client/src/app/shop/page.tsx
+++ client/src/app/shop/page.tsx
@@ -6,20 +6,12 @@ export default async function ShopPage() {
 
   return (
     <div className='p-4'>
-      <div className='my-8'>
-        <form>
-          <input
-            type='text'
-            name='search'
-            placeholder='Search products'
-            className='w-full p-2 border border-gray-300 rounded-md'
-          />
-        </form>
-      </div>
       <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4'>
-        {data.map((product: { id: number; name: string; price: number; image_url: string }) => (
-          <ProductCard key={product.id} product={product} />
-        ))}
+        {
+          data.map((product: { id: number; name: string; price: number; image_url: string }) => (
+            <ProductCard key={product.id} product={product} />
+          ))
+        }
       </div>
     </div>
   )
