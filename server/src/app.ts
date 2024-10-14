8c41c2e8a2d8e2573d8c8f410591bf65e49df19a
Iv44n <ivandev2oo6@gmail.com>
2024-10-13 23:07:31 -0500

    add requestLogger middleware to log incoming requests

--- server/src/app.ts
+++ server/src/app.ts
@@ -4,8 +4,14 @@ import express from 'express'
 // IMPORT ROUTES
 import productRouters from './routes/productRoutes'
 
+// IMPORT MIDDLEWARES
+import { requestLogger } from './middlewares/requestLogger'
+
 const app = express()
+
+// MIDDLEWARES
 app.use(express.json())
+app.use(requestLogger)
 
 // ROUTES
 app.use('/api/products', productRouters)
