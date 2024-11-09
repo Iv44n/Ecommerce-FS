59f036eded01bdb856dd2e9482b193bf49501f10
Iv44n <ivandev2oo6@gmail.com>
2024-11-09 10:42:04 -0500

    refactor: wrap product controller methods with error handler

--- server/src/controllers/productController.ts
+++ server/src/controllers/productController.ts
@@ -1,36 +1,23 @@
 import { Request, Response } from 'express'
 import { ProductService } from '../services/productService'
+import catchErrors from '../utils/catchErrors'
 
 export class ProductController {
 
-  static async getAllProducts(req: Request, res: Response) {
+  static getAllProducts = catchErrors(async (req: Request, res: Response) => {
     const limit = parseInt(req.query.limit as string) || 10
     const offset = parseInt(req.query.offset as string) || 0
 
-    try {
-      const products = await ProductService.getAllProducts({
-        limit,
-        offset
-      })
+    const products = await ProductService.getAllProducts({
+      limit,
+      offset
+    })
 
-      return res.json(products)
-    } catch (error) {
-      console.log(error)
-      return res.status(400).json({
-        error: 'Cannot get products'
-      })
-    }
-  }
+    return res.json(products)
+  })
 
-  static async getProduct(req: Request, res: Response) {
-    try {
-      const product = await ProductService.getProduct({ id: req.params.id })
-      return res.json(product)
-    } catch (error) {
-      console.log(error)
-      return res.status(400).json({
-        error: 'Cannot get product'
-      })
-    }
-  }
+  static getProduct = catchErrors(async (req: Request, res: Response) => {
+    const product = await ProductService.getProduct({ id: req.params.id })
+    return res.json(product)
+  })
 }
