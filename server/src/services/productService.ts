5d63c7e7bb0e40b2bbf045fd707159e55615d696
Iv44n <ivandev2oo6@gmail.com>
2024-11-13 16:51:31 -0500

    refactor: rename getProduct to getProductById and adjust parameters in ProductService

--- server/src/services/productService.ts
+++ server/src/services/productService.ts
@@ -3,18 +3,11 @@ import { Product } from '../types/product'
 
 export class ProductService {
 
-  static async getAllProducts({ limit = 10, offset = 0 }: { limit?: number, offset?: number }): Promise<Product[]> {
-    return await ProductModel.getAllProducts({
-      limit,
-      offset
-    })
+  static async getAllProducts({ limit = 10, offset = 0 }: { limit: number, offset: number }): Promise<Product[]> {
+    return await ProductModel.getAllProducts({ limit, offset })
   }
 
-  static async getProduct({ id }: { id: string }): Promise<Product> {
-    if (!id) {
-      throw new Error('Missing id')
-    }
-
-    return ProductModel.getProduct({ id })
+  static async getProductById({ id }: { id: string | number }): Promise<Product> {
+    return ProductModel.getProductById({ id })
   }
 }
