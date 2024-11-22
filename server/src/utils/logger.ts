06635d599897fd6e943576681b503e75f46da0c0
Iv44n <ivandev2oo6@gmail.com>
2024-11-21 21:45:17 -0500

    refactor: improve logger typings and ensure safer argument handling

--- server/src/utils/logger.ts
+++ server/src/utils/logger.ts
@@ -1,9 +1,11 @@
+import pc from 'picocolors'
+
 const info = (...args: unknown[]) => {
-  console.log(...args)
+  console.log(pc.blue(pc.bold('INFO:')), ...args)
 }
 
 const error = (...args: unknown[]) => {
-  console.error(...args)
+  console.error(pc.redBright(pc.bold('ERROR:')), ...args)
 }
 
 export const logger = { info, error }
