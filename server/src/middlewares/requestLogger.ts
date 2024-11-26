b366b7204f4497d5123dfb1b6b598c627ac27080
Iv44n <ivandev2oo6@gmail.com>
2024-11-25 22:16:00 -0500

    filter sensitive data in requestLogger

--- server/src/middlewares/requestLogger.ts
+++ server/src/middlewares/requestLogger.ts
@@ -1,11 +1,20 @@
 import { RequestHandler } from 'express'
 import { logger } from '../utils/logger'
 
+const filterSensitiveData = (body: any) => {
+  const bodyCopy = { ...body }
+
+  if (bodyCopy.password) bodyCopy.password = '***'
+  if (bodyCopy.confirmPassword) bodyCopy.confirmPassword = '***'
+
+  return bodyCopy
+}
+
 export const requestLogger: RequestHandler = (req, _res, next) => {
   logger.info('------------------------')
   logger.info('METHOD: ', req.method)
   logger.info('PATH: ', req.path)
-  logger.info('BODY: ', req.body)
+  logger.info('BODY: ', filterSensitiveData(req.body))
   logger.info('------------------------')
 
   next()
